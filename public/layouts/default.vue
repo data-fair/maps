307f5e12d0eda61d88b242cd54e68f61c78ed159
Tom BANCHEREAU <banchereau.tom@gmail.com>
2021-10-22 09:31:28 +0200

    feat: add a simple nav bar

--- public/layouts/default.vue
+++ public/layouts/default.vue
@@ -1,6 +1,20 @@
 <template>
   <v-app>
     <v-main>
+      <v-app-bar>
+        <!-- <v-btn text :to="{name:'index'}">
+          Map
+        </v-btn> -->
+        <v-btn text :to="{name:'styles'}">
+          Styles
+        </v-btn>
+        <v-btn text :to="{name:'tilesets'}">
+          Tilesets
+        </v-btn>
+        <v-btn text :to="{name:'render-demo'}">
+          Render-demo
+        </v-btn>
+      </v-app-bar>
       <nuxt />
     </v-main>
   </v-app>
