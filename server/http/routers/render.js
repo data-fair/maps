e910493a9534b1d06d927b638e72b696e859304a
Tom BANCHEREAU <banchereau.tom@gmail.com>
2021-10-22 09:30:01 +0200

    feat: render can load data from query parameters

--- server/http/routers/render.js
+++ server/http/routers/render.js
@@ -1,6 +1,6 @@
 
 const bboxUtils = require('../../utils/bbox')
-
+const wkx = require('wkx')
 const router = module.exports = require('express').Router()
 router.param('style', require('../params/style'))
 
@@ -22,15 +22,33 @@ async function getOrPost(req, res) {
   const mapOptions = { width, height }
   const padding = query.padding ? parseFloat(query.padding) : undefined
 
+  // Load data
+  let geojson
+  if (body && body.sources && body.layers) {
+    req.style = JSON.parse(JSON.stringify(req.style))
+    req.style.sources = Object.assign(req.style.sources || {}, body.sources)
+    req.style.layers = req.style.layers.concat(body.layers)
+    geojson = body.sources
+  }
+
+  if (query['data-sources']) {
+    req.style = JSON.parse(JSON.stringify(req.style))
+
+    query['data-sources'].split(',').filter(d => query[d]).forEach((dataSource) => {
+      req.style.sources[dataSource] = req.style.sources[dataSource] || {
+        type: 'geojson',
+      }
+      req.style.sources[dataSource].data = wkx.Geometry.parse(Buffer.from(query[dataSource], 'base64url')).toGeoJSON()
+      geojson = req.style.sources[dataSource]
+    })
+  }
+
   // Center and zoom
 
   if (query.lon && query.lat && query.zoom) {
     const lon = parseFloat(query.lon)
-    if (isNaN(lon)) return res.status(400).send('query.lon is not a valid float')
     const lat = parseFloat(query.lat)
-    if (isNaN(lat)) return res.status(400).send('query.lat is not a valid float')
     const zoom = parseFloat(query.zoom)
-    if (isNaN(zoom)) return res.status(400).send('query.zoom is not a valid float')
     mapOptions.center = [lon, lat]
     mapOptions.zoom = zoom
   } else if (query.bbox) {
@@ -38,28 +56,24 @@ async function getOrPost(req, res) {
     if (bbox.length !== 4 || bbox.some(isNaN)) return res.status(400).send('query.bbox is not a valid bounding box')
     mapOptions.center = bboxUtils.getCenter(bbox)
     mapOptions.zoom = bboxUtils.getZoom({ width, height, padding, bbox })
-  } else if (body) { // auto
-    const bbox = bboxUtils.getBBoxFromGeojsonStyleSource(body.data)
+  } else if (geojson) { // auto
+    const bbox = bboxUtils.getBBoxFromGeojsonStyleSource(req.style.sources)
     mapOptions.center = bboxUtils.getCenter(bbox)
     mapOptions.zoom = bboxUtils.getZoom({ width, height, padding, bbox })
   } else {
-    return res.status(400).send()
+    return res.status(400).send('Nothing to center on')
   }
 
+  if (isNaN(mapOptions.center[0])) return res.status(400).send('lon is not a valid float')
+  if (isNaN(mapOptions.center[1])) return res.status(400).send('lat is not a valid float')
+  if (isNaN(mapOptions.zoom)) return res.status(400).send('zoom is not a valid float')
   //
-
-  if (body && body.data && body.layers) {
-    req.style = JSON.parse(JSON.stringify(req.style))
-    req.style.sources = Object.assign(req.style.sources || {}, body.data)
-    req.style.layers = req.style.layers.concat(body.layers)
-  }
-
   // if (query.debug) {
   //   return res.send({ query, body, mapOptions, params: req.params })
   //   // return res.send(req.style)
   // }
   try {
-    const { buffer, info } = await req.app.get('renderer').render(req.style, mapOptions, { format: 'png' }, { cookie: req.headers.cookie, publicBaseUrl: req.publicBaseUrl })
+    const { buffer/*, info */ } = await req.app.get('renderer').render(req.style, mapOptions, { format: 'png' }, { cookie: req.headers.cookie, publicBaseUrl: req.publicBaseUrl })
 
     if (!buffer) return res.status(404).send('Not found')
     res.set({
