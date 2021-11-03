23d77308425f78cc90bcf4c1078589c34473a483
Tom BANCHEREAU <banchereau.tom@gmail.com>
2021-11-03 12:21:20 +0100

    fix: nuxt publicPath

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ module.exports = {
   buildDir: 'nuxt-dist',
   build: {
     // cache: true,
-    publicPath: config.publicUrl + '_nuxt/',
+    publicPath: config.publicUrl + '/_nuxt/',
     transpile: [/@koumoul/], // Necessary for "à la carte" import of vuetify components
     extend (config, { isServer, isDev, isClient }) {
       // Ignore all locale files of moment.js, those we want are loaded in plugins/moment.js
