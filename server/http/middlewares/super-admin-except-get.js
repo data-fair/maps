74248c013fc37c6721dee9ec0de696181724396c
Tom BANCHEREAU <banchereau.tom@gmail.com>
2021-11-15 11:58:51 +0100

    fix: superAdmin middleware response

--- server/http/middlewares/super-admin-except-get.js
+++ server/http/middlewares/super-admin-except-get.js
@@ -7,7 +7,7 @@ if (!basePath.endsWith('/')) basePath += '/'
 
 module.exports = asyncWrap(async (req, res, next) => {
   if (req.method === 'GET') return next()
-  if (!req.user) return res.status(401).send()
-  if (!req.user.adminMode) return res.status(403).send()
+  if (!req.user) return res.status(401).send('SuperAdmin required')
+  if (!req.user.adminMode) return res.status(403).send('SuperAdmin required')
   next()
 })
